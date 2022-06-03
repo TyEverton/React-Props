28b6e454e3e5235cc5ccdfe486225b6f0077bf20
Ty <everton_915@yahoo.com>
2022-06-03 14:03:01 -0600

    adding notes

--- src/screens/news-feed-screen/news-feed-screen.tsx
+++ src/screens/news-feed-screen/news-feed-screen.tsx
@@ -2,15 +2,10 @@ import { Input, Button } from 'antd'
 import { useState } from 'react'
 import Tweet from '../../components/tweet'
 
-// First step is to build out the components on each page
-// 1) Included within the component on this particular screen are the textfield and the save button
-// 2) add an onClick or onSubmit on the save button and test it with console.log
-// 3) add an inputChange on the textfield component, and console.log the inputs
-// 4)create useState for the newTweets that the user inputs (this sets the newTweet value to the updated users value)
-// 5)create useState for listOfTweets that will store the list of the tweets users have input and saved (listOfTweets will be updated with the users newTweet because the onClick will call newTweet, after spreading/copying the listOfTweets)
-// 6)use props to pass the tweetTexts to the tweet component (set type as an empty a string and an empty array)
-// 7)
 
+// Props are a way to pass data from one component (parent) to another component (child)
+
+// In this example - news-feed-screen is the parent component, while tweet is the child component
 function FeedScreen() {
   const [newTweet, setNewTweet] = useState<string | undefined>()
   const [listOfTweets, setListOfTweets] = useState<string[]>([
@@ -21,11 +16,8 @@ function FeedScreen() {
     setNewTweet(newValue)
   }
   
-  // let array = ['firstItem', 'secondItem', 'thirdItem']
   const submitClick = () => {
     if (newTweet) setListOfTweets([...listOfTweets, newTweet])
-
-
   }
 
   return (
